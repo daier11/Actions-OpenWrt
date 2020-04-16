3934330ac0cabb49a1531eb695c7ec8c43e0469c
shaoyou11 <shaobinyx@gmail.com>
2020-04-16 23:52:02 +0800

    Update diybeta.sh

--- diybeta.sh
+++ diybeta.sh
@@ -9,3 +9,4 @@
 sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan 
+git clone https://github.com/fw876/helloworld/tree/master/luci-app-ssr-plus package/luci-app-ssr-plus
