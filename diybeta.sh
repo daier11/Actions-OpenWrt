63da8a1c4fa8e4a8c0b39ef79e9362e425da37c7
shaoyou11 <shaobinyx@gmail.com>
2020-04-17 01:22:35 +0800

    Update diybeta.sh

--- diybeta.sh
+++ diybeta.sh
@@ -9,8 +9,8 @@
 sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan 
-git clone https://github.com/fw876/helloworld feeds/helloworl
-#sed -i '/helloworld/ a src-git helloworld https://github.com/fw876/helloworld ' feeds.conf.default
+#git clone https://github.com/fw876/helloworld feeds/helloworl
+sed -i '/helloworld/ a src-git helloworld https://github.com/fw876/helloworld ' feeds.conf.default
 #git clone https://github.com/fw876/helloworld/tree/master/luci-app-ssr-plus package/luci-app-ssr-plus
 ./scripts/feeds update -a
 ./scripts/feeds install -a
