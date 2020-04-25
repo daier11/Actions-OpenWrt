06affd032afcb6b42eb4754d598319e1d006b8cc
shaoyou11 <shaobinyx@gmail.com>
2020-04-25 16:39:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,14 @@
 sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 # git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan 
+git clone https://github.com/yangsongli/luci-theme-atmaterial package/luci-theme-atmaterial
+#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan 
+#git clone https://github.com/fw876/helloworld feeds/helloworl
+sed -i '/helloworld/ a src-git helloworld https://github.com/fw876/helloworld ' feeds.conf.default
+#git clone https://github.com/fw876/helloworld/tree/master/luci-app-ssr-plus package/luci-app-ssr-plus
+./scripts/feeds update -a
+./scripts/feeds install -a
+#sed -i 's/1.1.1.1/18.180.163.163/g' package/lean/luci-app-nps/root/etc/config/nps
+#sed -i 's/option crypt '1'/option crypt '0'/g' package/lean/luci-app-nps/root/etc/config/nps
